0f59bf7f8d3d34e7ad46965d67b1a269578ada59
依田葉月 MacPro <yodahazuki@gmail.com>
2014-04-24 13:25:14 +0900

    initialCommit

---
+++ ETea/src/VecLine.java
@@ -0,0 +1,19 @@
+
+public class VecLine{
+
+	public int x1, x2;
+	public int y1, y2;
+	
+	public VecLine(int x1, int y1, int x2, int y2) {
+		// TODO Auto-generated constructor stub
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+	
+	
+
+	
+	
+}
